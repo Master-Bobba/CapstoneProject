103c16888b84da78833de48afa57fac58b5b3479
bilal-a-khan <142792067+bilal-a-khan@users.noreply.github.com>
2024-02-05 15:47:14 +0000

    Add ArtistList, Artist and DisplayArtist components

---
+++ frontend/src/ArtistList.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import DisplayArtist from "./DisplayArtist";
+
+const ArtistList = () => {
+
+    const [artistData, setArtistData] = useState(null);
+    const endPoint = 'http://localhost:8080/artists';
+    
+    useEffect(() =>{
+        const fetchArtists = async() => {
+            try {
+                setArtistData(
+                    await fetch(endPoint)
+                    .then((response) => response.json())
+                )
+            } catch (error){
+                console.log('Some error fetching artists' + error);
+            }
+        }
+        fetchArtists();
+    }, []);
+
+
+    return (
+        <div>
+            { artistData ? (
+                <div>
+                    {artistData.map((artist) => (
+                        <DisplayArtist artist= {artist}/>
+                    ))}
+                </div>
+            ) : (
+                <div>
+                    Loading Artists...
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ArtistList;
