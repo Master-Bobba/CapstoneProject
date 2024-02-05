8b8c6d5c6696df6467d72d809f5c6a4157f7c30d
Master-Bobba <bayvazov94@gmail.com>
2024-02-05 18:21:39 +0000

    fix to musuem deletion

--- frontend/src/DisplayMuseum.js
+++ frontend/src/DisplayMuseum.js
@@ -5,12 +5,14 @@ function DisplayMuseum (props) {
     const endPoint = 'http://localhost:8080/museum/';
 
     const handleDelete = async (event) =>{
+        document.getElementById(''+props.museum.id).remove();
         try {
             const options = {method:'DELETE'};
             const response = await fetch(endPoint + props.museum.id, options);
         } catch (error) {
             console.error('Error fetching data: ', error);
         }
+        
     }
 
     const handleEdit = (event) => {
