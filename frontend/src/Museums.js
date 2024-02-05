98b38f6dad2d3a00aaa87c56eaa2dfc860deef65
Master-Bobba <bayvazov94@gmail.com>
2024-02-05 13:27:39 +0000

    museum.js

--- frontend/src/Museums.js
+++ frontend/src/Museums.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Museums = () => {
+
+    const [museumData, setMuseumData] = useState(null);
+    const endPoint = 'http://localhost:8080/museum';
+    
+    useEffect(() =>{
+        const fetchMeseums = async() => {
+            try {
+                setMuseumData(
+                    await fetch(endPoint)
+                    .then((response) => response.json())
+                )
+            } catch (error){
+                console.log('Some error fetching museums' + error);
+            }
+        }
+        fetchMeseums();
+    }, []);
+
+
     return (
         <div>
-            Museums
+            <h3>Museums </h3>
+            { museumData ? (
+                <div>
+                    {museumData.map((museum) => (
+                        <div>
+                            <p>Museum Name: {museum.name}</p>
+                            <p>Location: {museum.location.city}, {museum.location.country}</p>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <div>
+                    Loading Meseums...
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Museums;
