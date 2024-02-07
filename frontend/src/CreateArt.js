c73c2c6008bec0c8e4dbafa428edf9c12ce4c74c
Sarah-Irvine <123417453+Sarah-Irvine@users.noreply.github.com>
2024-02-07 10:57:57 +0000

    Update CreateArt.js

--- frontend/src/CreateArt.js
+++ frontend/src/CreateArt.js
@@ -8,26 +8,35 @@ const CreateArt = () => {
   const handleSubmit = (event) => {
   
     const artStyle = document.querySelector('#artType').value;
+    console.log(artStyle);
     const endPoint = process.env.REACT_APP_SPRING_URL + '/' + artStyle;
+    console.log(endPoint);
     
     var name = document.querySelector('#artPiece').value;
 
-    const selectElement = document.querySelector('#artist.form-input');
-    const selectedIndex = selectElement.selectedIndex;
-    const selectedOption = selectElement.options[selectedIndex];
-    const selectedId = selectedOption.querySelector('p').getAttribute('id');
-    console.log('Selected id:', selectedId);
+    const selectElementArtist = document.querySelector('#artist.form-input');
+    const selectedIndexArtist = selectElementArtist.selectedIndex;
+    const selectedOptionArtist = selectElementArtist.options[selectedIndexArtist];
+    const selectedIdArtist = selectedOptionArtist.querySelector('p').getAttribute('id');
+    console.log('Selected Artist id:', selectedIdArtist);
+
+    const selectElementMuseum = document.querySelector('#museum.form-input');
+    const selectedIndexMuseum = selectElementMuseum.selectedIndex;
+    const selectedOptionMuseum = selectElementMuseum.options[selectedIndexMuseum];
+    const selectedIdMuseum = selectedOptionMuseum.querySelector('p').getAttribute('id');
+    console.log('Selected Museum id:', selectedIdMuseum);
 
     fetch(endPoint, {
         method: 'post',
         headers: {'Content-Type' : 'application/json'},
         body: JSON.stringify({
             "name": document.querySelector('#artPiece').value,
-            "artist": {"id": selectedId},
+            "artist": {"id": selectedIdArtist},
             "medium": document.querySelector('#medium').value.toUpperCase(),
-            "museum": parseInt(document.querySelector('#museum').value),
+            "museum": {"id": selectedIdMuseum},
             "yearCompleted": parseInt(document.querySelector('#yearCompleted').value),
-            "backStory": document.querySelector('#backStory').value
+            "backStory": document.querySelector('#backStory').value,
+            "style": document.querySelector('#style').value.toUpperCase()
         })
     });
     
@@ -47,6 +56,18 @@ const CreateArt = () => {
                   <option value="painting">Painting</option>
                 </select>
               </label>
+              <label>
+                Style:
+                <select class="form-input" id="style">
+                  <option value="defaultStyle">-select-</option>
+                  <option value="sculpture">SCULPTURE</option>
+                  <option value="impressionist">IMPRESSIONIST</option>
+                  <option value="abstract">ABSTRACT</option>
+                  <option value="renaissance">RENAISSANCE</option>
+                  <option value="romanticism">ROMANTICISM</option>
+                </select>
+              </label>
+              <br />
               <label>
                 Art Piece: 
                 <input class="form-input" id="artPiece" name="name" type="text"/>
