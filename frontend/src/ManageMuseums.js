2d8810d82368dbc509452616307417a33f80f610
Sarah-Irvine <123417453+Sarah-Irvine@users.noreply.github.com>
2024-02-08 15:06:11 +0000

    Update ManageMuseums.js

--- frontend/src/ManageMuseums.js
+++ frontend/src/ManageMuseums.js
@@ -8,11 +8,10 @@ const ManageMuseums = () => {
         <div class="main-div-layout">
             <section class="page_title">
                 <h2 class="page-title__header">
-                Museums
+                Colleague Hub - Manage Museums
                 </h2>
                 <h3 class="page-title__text">
-                    Select a Museum to view opening hours, address & contact details;
-                    purchase tickets; or, find out a little more about the art collections.
+                    Manage our Museums below
                 </h3>
                 <SearchBar searchFor = "museum"/>
             </section>
