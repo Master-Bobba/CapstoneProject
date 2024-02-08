f86359a14a8ac8413e84c74ea53da42481c89b17
Master-Bobba <bayvazov94@gmail.com>
2024-02-08 09:43:24 +0000

    change to url in home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState }  from "react";
-// import Carousel from "react-bootstrap/Carousel";
+import Carousel from "react-bootstrap/Carousel";
 
-// const images = require.context('./CarouselImages', true);
-// const imageList = images.keys().map(image => images(image));
+const images = require.context('./CarouselImages', true);
+const imageList = images.keys().map(image => images(image));
 
 const Home = () => {
     const [artData, setArtData] = useState(null);
@@ -10,7 +10,7 @@ const Home = () => {
     useEffect (() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('http://localhost:8080/art');
+                const response = await fetch(process.env.REACT_APP_SPRING_URL +'/art');
                 const data = await response.json();
                 console.log(data);
                 setArtData(data);
@@ -23,28 +23,24 @@ const Home = () => {
     );
 
     return(
-        <div>
-            -- HOME PAGE CAROUSEL UNDER CONSTRUCTION --
-        </div>
-        
-        // artData ? (
-        //     <div>
-        //         <Carousel>
-        //         {artData.map((art) => (
-        //             <Carousel.Item>
-        //                 <div key={art.id}>
-        //                     <div>
-        //                     <p>Title: {art.name}</p>
-        //                     <img src= {art.url} height={300} width={500} />
-        //                     </div>
-        //                 </div>
-        //             </Carousel.Item>
-        //             ))}
-        //         </Carousel>
-        //     </div>
-        // ) : (
-        //     <p>Loading...</p>
-        // )
+        artData ? (
+            <div>
+                <Carousel>
+                {artData.map((art) => (
+                    <Carousel.Item>
+                        <div key={art.id}>
+                            <div>
+                            <p>Title: {art.name}</p>
+                            <img src= {art.url} height={300} width={500} />
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    ))}
+                </Carousel>
+            </div>
+        ) : (
+            <p>Loading...</p>
+        )
     );
 };
 
