fd87d53a87d196cdb49231ca0cbd2fc15a4688ac
Master-Bobba <bayvazov94@gmail.com>
2024-02-08 15:21:06 +0000

    some changes to home page css

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState }  from "react";
 import Carousel from "react-bootstrap/Carousel";
+import './home.css';
 
 const Home = () => {
     const [artData, setArtData] = useState(null);
@@ -22,22 +23,30 @@ const Home = () => {
     return(
         artData ? (
             <div>
-                <Carousel>
-                {artData.map((art) => (
-                    <Carousel.Item>
-                        <div key={art.id}>
-                            <div>
-                            <p>Title: {art.name}</p>
-                            <img src= {art.url} width={500} />
+                <h2 class="welcome">Welcome to our museum!</h2>
+                <div class="caro">
+                    <Carousel>
+                    {artData.map((art) => (
+                        <Carousel.Item>
+                            <div key={art.id}>
+                                <div class="mainContent">
+                                    <p class="title">{art.name} by { art.artist.name}</p>
+                                    <img class="mainContent" src= {art.url} width={500} />
+                                    
+                                </div>
+                                
                             </div>
-                        </div>
-                    </Carousel.Item>
-                    ))}
-                </Carousel>
-                <h2>Welcome to our museum!</h2>
-                <h4>Use the options on the top bar on the page</h4> 
-                <h4>to create or view art, artists, or museums!</h4>
-                <h4>See the "About us" link below for more information</h4>
+                        </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </div>
+                
+                <div class="mainContent">
+                    <h4 class="frontText"></h4> 
+                    <h3 class="frontText">Use the options on the top bar on the page to create or view art, artists, or museums!</h3>
+                    <h3 class="frontText"> See the "About us" link below for more information</h3>
+                </div>
+                
             </div>
         ) : (
             <p>Loading...</p>
